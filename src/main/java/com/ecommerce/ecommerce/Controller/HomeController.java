551f645f66d3a6f4ef824a8a5f25ccddf93401f0
eduardo <edu.alamillo.isc@gmail.com>
2024-09-11 03:15:57 -0600

    la cantidad y buscar producto en el homecontroller para añadir al carrito

--- src/main/java/com/ecommerce/ecommerce/Controller/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/Controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.ecommerce.Controller;
 
+import com.ecommerce.ecommerce.Model.DetalleOrden;
+import com.ecommerce.ecommerce.Model.Orden;
 import com.ecommerce.ecommerce.Model.Producto;
 import com.ecommerce.ecommerce.Service.ProductoService;
 import org.slf4j.Logger;
@@ -7,10 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -21,6 +23,12 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+
+    //para almacenar los detalles de la orden
+    List<DetalleOrden> detalle = new ArrayList<DetalleOrden>();
+
+    //datos de la orden
+    Orden orden = new Orden();
 
     @GetMapping("")
     public String home(Model model){
@@ -40,5 +48,18 @@ public class HomeController {
         model.addAttribute("producto", producto);
 
         return "administrador/usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal=0;
+
+        Optional<Producto> optionalProducto = productoService.getProducto(id);
+        log.info("Producto añadido: {}", optionalProducto.get());
+        log.info("cantidad: {}", cantidad);
+
+        return "administrador/usuario/carrito";
     }
 }
