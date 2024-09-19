457509748b66e44a154280e2648b8c8996ff4c42
eduardo <edu.alamillo.isc@gmail.com>
2024-09-19 01:43:48 -0600

    funcion generar numero orden y finAll

--- src/main/java/com/ecommerce/ecommerce/Service/IOrdenService.java
+++ src/main/java/com/ecommerce/ecommerce/Service/IOrdenService.java
@@ -3,7 +3,10 @@ package com.ecommerce.ecommerce.Service;
 import com.ecommerce.ecommerce.Model.Orden;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden Save(Orden orden);
 }
